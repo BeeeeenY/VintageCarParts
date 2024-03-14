5c48ecb06f4d11baa582bf1c8b634281760331fa
charmaine <wltan.2022@smu.edu.sg>
2024-03-15 02:01:15 +0800

    Update logintemplate.py

--- logintemplate.py
+++ logintemplate.py
@@ -2,41 +2,26 @@ from flask import Flask, request, redirect, url_for, session, render_template, f
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from invokes import invoke_http
-from os import environ
 import bcrypt
+import requests
 
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Configure SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/Authentication'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
-
-app.config['SQLALCHEMY_BINDS'] = {
-    'users': 'mysql+mysqlconnector://root@localhost:3306/Users'
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/Authentication'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Define your UserAuth model
 class UserAuth(db.Model):
-    __tablename__ = 'UserAuth'  # Specify the correct table name here
+    __tablename__ = 'UserAuth'
 
     AuthID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     UserID = db.Column(db.Integer, nullable=False)
     Email = db.Column(db.String(255), unique=True, nullable=False)
     PasswordHash = db.Column(db.String(255), nullable=False)
-
-class Users(db.Model):
-    __bind_key__ = 'users'
-    __tablename__ = 'users'  # Ensure this matches the actual table name in your database
-
-    UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    Name = db.Column(db.String(255), nullable=False)
-    Phone = db.Column(db.String(20))
-    Age = db.Column(db.Integer)
-    Country = db.Column(db.String(255))
 
 # Routes for authentication
 @app.route('/', methods=['GET', 'POST'])
@@ -53,9 +38,25 @@ def login():
             hashed_password_bytes = hashed_password.encode('utf-8')
             if bcrypt.checkpw(password_bytes, hashed_password_bytes):
                 print("Password match!")
+
                 session['email'] = email
                 db.session.commit()
-                return invoke_http(url='http://127.0.0.1:5002/', method='GET', redirect_url='http://127.0.0.1:5002/')
+
+                email = session.get('email')
+                print(email)
+                    
+                # Fetch user ID of user logged in
+                loggedin_user_id = db.session.query(UserAuth.UserID).filter(UserAuth.Email == email).scalar()
+                print(loggedin_user_id)
+
+                session['loggedin_user_id'] = loggedin_user_id
+                db.session.commit()
+
+                redirect_url = 'http://127.0.0.1:5002/'
+    
+                # Redirect to the constructed URL
+                return redirect(redirect_url)
+
             else:
                 error = 'Invalid email/password'
         else:
@@ -94,14 +95,17 @@ def register():
             flash('Email already registered.')
             return redirect(url_for('register'))
         
-        new_user = Users(Name=name, Phone=phone, Age=age, Country=country)
-        db.session.add(new_user)
-        db.session.commit()
-        
-        new_user_id = new_user.UserID
+        add_user_url = 'http://127.0.0.1:5004/add_user'
+        add_user_params = {'name': name, 'phone' : phone, 'age' : age, 'country' : country}
+        add_user_response = requests.get(add_user_url, params=add_user_params)
+
+        if add_user_response.status_code == 200:
+            # Get the username from the response if needed
+            user_id = add_user_response.json().get('user_id')
+            print("user_id:", user_id)
         
         # Create a new user authentication record using the retrieved UserID
-        new_user_auth = UserAuth(UserID=new_user_id, Email=email, PasswordHash=hashed_password)
+        new_user_auth = UserAuth(UserID=user_id, Email=email, PasswordHash=hashed_password)
         db.session.add(new_user_auth)
         db.session.commit()
         

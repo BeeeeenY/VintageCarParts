6eb3ab3c464de184e3adbc050ba7b1ae16fd1771
AH MIN WEI <minwei.ah.2022@scis.smu.edu.sg>
2024-03-31 13:05:28 +0800

    payment swagger

--- payment/server/server.js
+++ payment/server/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config()
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -12,6 +14,9 @@ app.use(
 );
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+// Load your API definition file
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // const storeItems = new Map([
 //   [1, { priceInCents: 100000, name: "Aston Martin DB7 I6 engine parts" }],
@@ -19,6 +24,40 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 //   [3, { priceInCents: 20000, name: "Get an A for ESM" }],
 // ])
 const storeItems = new Map([])
+
+/**
+ * @swagger
+ * tags:
+ *   name: Store
+ *   description: Endpoints for managing store items and cart
+ */
+
+/**
+ * @swagger
+ * /items:
+ *   get:
+ *     summary: Get all items in the store
+ *     description: Retrieve all items available in the store
+ *     responses:
+ *       200:
+ *         description: A list of store items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The ID of the item
+ *                   name:
+ *                     type: string
+ *                     description: The name of the item
+ *                   priceInCents:
+ *                     type: integer
+ *                     description: The price of the item in cents
+ */
 
 // this endpoint to serve the items' data
 app.get("/items", (req, res) => {
@@ -28,6 +67,43 @@ app.get("/items", (req, res) => {
   }));
   res.json(itemsArray);
 });
+/**
+ * @swagger
+ * /add-to-cart:
+ *   post:
+ *     summary: Add an item to the cart
+ *     description: Add an item to the shopping cart with specified details
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               PartID:
+ *                 type: integer
+ *                 description: ID of the part being ordered
+ *               Price:
+ *                 type: number
+ *                 description: Price of the part being ordered
+ *               quantity:
+ *                 type: integer
+ *                 description: Quantity of the part being ordered
+ *               BuyerID:
+ *                 type: integer
+ *                 description: ID of the user placing the order
+ *               SellerID:
+ *                 type: integer
+ *                 description: ID of the seller
+ *               ProductName:
+ *                 type: string
+ *                 description: Optional name of the product
+ *     responses:
+ *       302:
+ *         description: Redirect to the cart page after adding the item to the cart
+ *       500:
+ *         description: Internal server error
+ */
 
 // To add in user
 app.post("/add-to-cart", (req, res) => {
@@ -48,11 +124,56 @@ app.post("/add-to-cart", (req, res) => {
   
 });
 
+/**
+ * @swagger
+ * /clear-cart:
+ *   post:
+ *     summary: Clear the cart
+ *     description: Clear all items from the shopping cart
+ *     responses:
+ *       200:
+ *         description: Cart cleared successfully
+ *       500:
+ *         description: Internal server error
+ */
+
+
 app.post("/clear-cart", (req, res) => {
   storeItems.clear(); // This clears the entire map, effectively emptying the cart
   console.log("Cart has been emptied");
   res.status(200).send("Cart cleared successfully");
 });
+
+/**
+ * @swagger
+ * /create-checkout-session:
+ *   post:
+ *     summary: Create a checkout session
+ *     description: Create a new checkout session for processing payments
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               items:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: ID of the item
+ *                     quantity:
+ *                       type: integer
+ *                       description: Quantity of the item
+ *     responses:
+ *       200:
+ *         description: URL for the checkout session
+ *       500:
+ *         description: Internal server error
+ */
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
@@ -81,4 +202,12 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 })
 
-app.listen(3000)
+
+// app.listen(3000)
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

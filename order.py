baaf0d5d47306e86220ecae704879119cf20186b
Chinyeeee <146084496+Chinyeeee@users.noreply.github.com>
2024-04-01 15:44:06 +0800

    Update order.py

--- order.py
+++ order.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/orders'
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     environ.get("dbURL") or "mysql+mysqlconnector://root@localhost:3306/orders"
 )

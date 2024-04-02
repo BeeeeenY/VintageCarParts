d6c22d9de21fe946918c1dedc38c2a4464373afb
charmaine <wltan.2022@smu.edu.sg>
2024-04-02 13:28:59 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+from flasgger import Swagger
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+app.config['SWAGGER'] = {
+    'title': 'chat microservice API',
+    'version': 1.0,
+    "openapi": "3.0.2",
+    'description': 'Allows renders of chat interface'
+}
+swagger = Swagger(app)
 
 @socketio.on("message")
 def sendMessage(message):
     send(message, broadcast=True)
-    # send() function will emit a message vent by default
 
 
 @app.route("/")
 def message():
+    """
+    This route renders a chat interface.
+
+    ---
+    responses:
+        200:
+            description: A chat interface is rendered successfully.
+    """
     return render_template("chat.html")
 
 
